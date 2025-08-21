c82cde349897372d9ada906a29bc3cb525ef5333
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-08-21 03:22:39 +0000

    Added user_id column to Boards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_21_030044) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_21_032156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_21_030044) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "listings", force: :cascade do |t|
