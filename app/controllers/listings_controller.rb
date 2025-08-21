7ac8cfb96726e0e5147a2d026aafd407e539bb28
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2025-08-21 04:08:55 +0000

    Saving user_id when listing is created

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -23,6 +23,7 @@ class ListingsController < ApplicationController
     the_listing.body = params.fetch("query_body")
     the_listing.expires_on = params.fetch("query_expires_on")
     the_listing.board_id = params.fetch("query_board_id")
+    the_listing.user_id = current_user.id
 
     if the_listing.valid?
       the_listing.save
